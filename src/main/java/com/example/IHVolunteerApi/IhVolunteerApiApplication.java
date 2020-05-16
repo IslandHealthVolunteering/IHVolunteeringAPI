b2e79651862b06b6b3eef29015fea68faba81c8a
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-05-16 16:29:31 -0700

    add boilerplate

---
+++ src/main/java/com/example/IHVolunteerApi/IhVolunteerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.IHVolunteerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhVolunteerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhVolunteerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IHVolunteerApi/IhVolunteerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IHVolunteerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhVolunteerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
