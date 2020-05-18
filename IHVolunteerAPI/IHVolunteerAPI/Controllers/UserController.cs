75adf8142a6a455810227e7ba5bb1d43735641c2
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-05-17 20:01:29 -0700

    remove spring boot api and replace with dotnet core ef

---
+++ IHVolunteerAPI/IHVolunteerAPI/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IHVolunteerAPIData.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IHVolunteerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        readonly IHVolunteerAPIContext Context;
+
+        public UserController(IHVolunteerAPIContext context)
+            => Context = context;
+
+        [HttpGet]
+        public IActionResult GetUser()
+        {
+            var users = Context.User.ToList();
+
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser()
+        {
+            var user = new User()
+            {
+                Name = "Test User",
+                Email = "testuser@example.com",
+                VolunteerHours = 10,
+                Password = "verysecurepassword"
+            };
+
+            Context.Add(user);
+            Context.SaveChanges();
+
+            return Ok("Successfully created user with email " + user.Email);
+        }
+    }
+}
---
+++ IHVolunteerAPI/IHVolunteerAPIData/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IHVolunteerAPIData.Models
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int VolunteerHours { get; set; }
+
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
