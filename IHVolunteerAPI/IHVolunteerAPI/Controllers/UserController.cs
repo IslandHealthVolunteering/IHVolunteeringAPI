8bb6656a6fa8c9d7a0924f34aaaa936ad44dadbf
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-05-18 16:59:26 -0700

    aes encryption/decryption

--- IHVolunteerAPI/IHVolunteerAPI/Controllers/UserController.cs
+++ IHVolunteerAPI/IHVolunteerAPI/Controllers/UserController.cs
@@ -3,6 +3,9 @@ using IHVolunteerAPIData.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Security.Cryptography;
+using IHVolunteerAPI.Security;
+using System.Text;
 
 namespace IHVolunteerAPI.Controllers
 {
@@ -26,6 +29,33 @@ namespace IHVolunteerAPI.Controllers
             }
 
             return NoContent();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateUser()
+        {
+            string original = "Please let me dip them mannn";
+
+            // Create a new instance of the Aes
+            // class.  This generates a new key and initialization
+            // vector (IV).
+            using (Aes myAes = Aes.Create())
+            {
+                byte[] key = new byte[]
+                {
+                    145, 244, 24, 44, 98, 192, 234, 23,
+                    97, 88, 91, 246, 77, 59, 25, 56
+                };
+                myAes.Key = key;
+          
+                // Encrypt the string to an array of bytes.
+                byte[] encrypted = AES.EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
+
+                // Decrypt the bytes to a string.
+                string decrypted = AES.DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+            }
+
+            return Ok("done");
         }
 
         [HttpPost]
